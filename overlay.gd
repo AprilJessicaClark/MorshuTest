d32baee1840636042ec1032667e807037b314c01
AprilJessicaClark <apriljessicaclark@gmail.com>
2024-11-23 17:02:14 -0500

    added inventory overlay

--- overlay.gd
+++ overlay.gd
@@ -1,4 +1,3 @@
-@tool
 extends Node2D
 
 @export
@@ -7,6 +6,7 @@ var location : Node2D;
 @export
 var default_location : PackedScene
 
+	
 func move_to_location(destination : PackedScene):
 	var parent = location.get_parent();
 	parent.remove_child(location);
@@ -18,8 +18,18 @@ func move_to_location(destination : PackedScene):
 func _ready() -> void:
 	LocationSwapper.world = $World;
 	LocationSwapper.swap_location(default_location)
+	Dialogic.VAR.variable_changed.connect(update_inventory);
 
-
+func update_inventory(x):
+	var items : Array[String] = [];
+	var item_string = "None"
+	for item : String in Dialogic.VAR.Items.variables():
+		if Dialogic.VAR.Items.get(item):
+			items.append(item)
+	if len(items) > 0:
+		item_string = ", ".join(items)
+	$Items.text = item_string;
+		
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
